81c2d86ef36edf4bb0d515adf1d2fadc38643f7b
Rosu Liviu <rosuliviu10@gmail.com>
2025-06-27 19:40:32 +0300

    subiectul 1 rezolvat fara teorie

---
+++ rezolvari/subiectul 1/Region.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include "Weather.h"
+#include <vector>
+using namespace std;
+
+// (2p) To exemplify the use of a “has a” type 1: M relation (one to several) 
+// by defining an additional class to manage the weather conditions for a region with multiple locations. You can use a STL collection but it is not mandatory. 
+
+class Region{
+private:
+    string name;
+    //vector<Weather> weatherData; using STL
+    
+    //or classic array
+    Weather weatherData[3];
+    int numberOfWeather = 0;
+
+public:
+    
+
+    Region(){
+        name = "";
+        numberOfWeather = 0;
+    }
+
+    Region(string newName){
+        name = newName;
+        numberOfWeather = 0;
+    }
+
+    void addWeather(const Weather& w){
+        //if we use STL
+        // weatherData.push_back(w);    
+        
+        //if we use classic array
+        if(numberOfWeather < 3){
+            weatherData[numberOfWeather] = w;
+            numberOfWeather++;
+        }
+        else{
+            cout << "Array is full. Cannot add any more data" << endl;
+        }
+    }
+
+    //optional
+    void displayWeather(){
+        cout << "Region: " << name << endl;
+        for(int i = 0; i < numberOfWeather; i++){
+            cout << "Weather #" << (i+1) << " " << weatherData[i].getDate() << ", " << weatherData[i].getDescription() << ", ";
+            if(weatherData[i].getLocation() != nullptr){
+                cout << weatherData[i].getLocation() << endl;
+            }
+            else{
+                cout << "Unknown location";
+            }
+
+            float* temps = weatherData[i].getTemperature();
+            cout << "Temperatures: ";
+            for(int j = 0; j < weatherData[i].getNumberOfElements(); j++){
+                cout << temps[j] << " ";
+            }
+            cout << endl;
+            cout << endl;
+
+            delete[] temps;
+        }
+    }
+};
--- rezolvari/subiectul 1/main.cpp
+++ rezolvari/subiectul 1/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
 #include <string.h>
-#include "Weather.cpp"
-#include "Region.cpp"
+#include "Weather.h"
+#include "Region.h"
 
 int main(){
 
     //testing
     float temps[4] = {27.5, 29.3, 34.5, 36};
+    float temps2[4] = {23, 22.4, 21.3, 20.1};
     
-    
+    Weather w1("27/06/2025", temps, 4, "Really hot", (char*)"Bucharest");
+    Weather w2("28/06/2025", temps2, 4, "Sunny", (char*)"Brasov");
+
+    Weather w3 = w1;
+
+    Region region;
+    region.addWeather(w1);
+    region.addWeather(w2);
+    region.addWeather(w3);
+
+    region.displayWeather();
 
     return 0;
 }
