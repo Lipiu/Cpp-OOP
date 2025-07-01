bd232bc686f55040b6c6dd140411df87bd5473fc
Rosu Liviu <rosuliviu10@gmail.com>
2025-07-01 15:19:45 +0300

    am implementat si testat mai multi operatori va rog salvati-ma

--- rezolvari/subiectul 4/Book.h
+++ rezolvari/subiectul 4/Book.h
@@ -102,14 +102,43 @@ public:
         }
     }
 
+    //overload operator +
     friend void operator+(float price, Book& book){
         book.set_price(book.get_price() + price);
     }
 
+    //overload operator -
+    Book operator-(const Book& new_book) const{
+        Book copy = *this;
+        copy.price -= new_book.price;
+        return copy;
+    }
+
+    //overload operator *
+    Book operator*(const Book& new_book) const{
+        Book copy = *this;
+        copy.price *= new_book.price;
+        return copy;
+    }
+
+    //overload operator /
+    Book operator/(const Book& new_book) const{
+        Book copy = *this;
+        copy.price /= new_book.price;
+        return copy;
+    }
+
+    //overload operator !=
+    bool operator!=(const Book& new_book) const{
+        return this->price != new_book.price;
+    }
+
+    //overload operator <
     friend bool operator<(Book& book1, Book& book2){
         return book1.number_of_pages < book2.number_of_pages;
     }
 
+    //overload operator <<
     friend std::ostream& operator<<(std::ostream& os, const Book& book) {
         os << "Title: " << (book.title ? book.title : "(no title)") << " | "
            << "Author: " << (book.author ? book.author : "(no author)") << " | "
@@ -117,6 +146,83 @@ public:
            << "Price: " << book.price;
         return os;
     }
+
+    //overload operator >>
+    friend std::istream& operator>>(std::istream& in, Book& book){
+
+        std::cout << "Enter title: ";
+        in >> book.title;
+        in.ignore();
+
+        std::cout << "Enter author: ";
+        in >> book.author;
+        in.ignore();
+
+        std::cout << "Enter price: ";
+        in >> book.price;
+        while(book.price < 0){
+            std::cout << "Price cannot be negative. Try again: ";
+            in >> book.price;
+        }
+
+        if(book.price == 0){
+            std::cout << "You got it for free!" << std::endl;
+        }
+
+        in.ignore();
+        std::cout << "Thank you for your purchase!" << std::endl;
+        
+        return in;
+    }
+
+    //overloading == operator
+    bool operator==(const Book& new_book) const{
+        if(this == &new_book)
+            return true;
+        if(this->title == nullptr || new_book.title == nullptr)
+            return false;
+        if(strcmp(this->title, new_book.title) != 0)
+            return false;
+        if(strcmp(this->author, new_book.author) != 0)
+            return false;
+        if(this->number_of_pages != new_book.number_of_pages)
+            return false;
+        if(this->price != new_book.price)
+            return false;
+        
+        return true;
+    }
+
+    //overloading ++ prefix
+    Book& operator++(){
+        ++this->price += 100;
+        return *this;
+    }
+
+    //overloading ++ postfix
+    Book operator++(int){
+        Book copy = *this;
+        this->price += 150;
+        return copy;
+    }
+
+    //overloading -- prefix
+    Book& operator--(){
+        if(price > 0){
+            price -= 10;
+        }
+        else{
+            std::cout << "Price is negative. Exiting...";
+        }
+        return *this;
+    }
+
+    // overloading -- postfix
+    Book operator--(int){
+        Book copy = *this;
+        this->price -= 20;
+        return copy;
+    }    
 };
 
 int Book::book_counter_by_id = 0;
--- rezolvari/subiectul 4/main.cpp
+++ rezolvari/subiectul 4/main.cpp
@@ -13,13 +13,68 @@ int main(){
     lib.add_book(b2);
     lib.display_info();
 
+    //operator +
     std::cout << "\nTesting operator + " << std::endl;
 
     100.2 + b1;
     std::cout << "Price after increase: " << b1.get_price();
 
-    std::cout << "\n\n";
+    std::cout << "\n";
 
-    std::cout << "Testing overloaded <<:\n";
+    // operator -
+    std::cout << "\nTesting operator - " << std::endl;
+    Book b4 = b1 - b2;
+    std::cout << "Price of b1: " << b1.get_price() << std::endl;
+    std::cout << "Price of b2: " << b2.get_price() << std::endl;
+    std::cout << "Price of b4 (b1 - b2): " << b4.get_price() << std::endl;
+
+    std::cout << std::endl;
+
+    // am dat overload si la operatorii *, /, != dar testarea e la fel
+
+    // operator << 
+    std::cout << "Testing overloaded << operator:\n";
     std::cout << b1;
+
+    // std::cout << "\n\nTesting overloaded >> operator: \n";
+    // std::cin >> b1;
+
+    // == operator
+    Book b3 = b2;
+    std::cout << "\n\nTesting overloaded == operator: \n";
+    if(b3 == b2){
+        std::cout << "The books are the same";
+    }
+    else{
+        std::cout << "The books are different";
+    }
+
+    // ++ prefix
+    std::cout << "\n\nTesting overloaded ++ prefix:\n";
+    std::cout << "Before prefix ++ the price of b2 was: " << b2.get_price() << std::endl;
+    
+    ++b2;
+    std::cout << "After prefix ++ the price of b2 is: " << b2.get_price();
+
+    // ++ postfix
+    std::cout << "\n\nTesting overloaded ++ postfix:\n";
+    Book old = b1++;
+
+    std::cout << "Before postfix ++ the price of b1 was: " << old.get_price() << std::endl;
+    std::cout << "After postfix ++ the price of b1 is: " << b1.get_price() << std::endl;
+
+    // -- prefix
+    std::cout << "\nTesting overloaded -- prefix:\n";
+    std::cout << "Before prefix -- the price of b3 was: " << b3.get_price() << std::endl;
+    
+    --b3;
+    std::cout << "After prefix -- the price of b3 is: " << b3.get_price();
+
+
+    // -- postfix
+    std::cout << "\n\nTesting overloaded -- postfix:\n";
+    Book old2 = b3--;
+
+    std::cout << "Before postfix -- the price of b3 was: " << old2.get_price() << std::endl;
+    std::cout << "After postfix -- the price of b3 is: " << b3.get_price() << std::endl;
 }
